25de615c789a543f414b669f4cdc608524cf609b
jmX86 <jm54307@fer.hr>
2026-01-13 23:38:53 +0100

    DataServer added

--- src/main/java/hr/fer/dippro/Main.java
+++ src/main/java/hr/fer/dippro/Main.java
@@ -7,6 +7,8 @@ import hr.fer.dippro.device.DeviceStatusUpdater;
 import hr.fer.dippro.mqtt.MqttDetectionReceiver;
 import hr.fer.dippro.mqtt.MqttStatusReceiver;
 import hr.fer.dippro.mqtt.TopicData;
+import hr.fer.dippro.server.DataServer;
+import io.javalin.Javalin;
 
 import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -17,38 +19,19 @@ import java.util.concurrent.LinkedBlockingQueue;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+//    private static final String brokerURL = "tcp://localhost:1884";
     private static final String brokerURL = "tcp://broker:1884";
     // "tcp://Mac-mini-od-Josip.local:1884"
     private static final String deviceDetection1 = "ESP32_154130/det";
     private static final String deviceDetection2 = "ESP32_157A00/det";
 
+    private static final int dataServerStartRow = 5001;
+
     public static void main(String[] args) {
         BlockingQueue<Long> queueTimestamp1 = new LinkedBlockingQueue<>();
         BlockingQueue<Long> queueTimestamp2 = new LinkedBlockingQueue<>();
 
-//        BlockingQueue<TopicData> queueDeviceStatusTopicData = new LinkedBlockingQueue<>(20);
-//        BlockingQueue<DeviceData> queueDeviceStatusUpdate = new LinkedBlockingQueue<>(20);
         BlockingQueue<Double> queueDeviceSpeed = new ArrayBlockingQueue<>(100);
-
-//        Map<String, DeviceStatus> devicesStatus = new ConcurrentHashMap<>();
-//        Map<String, DeviceData> deviceDataCache = new ConcurrentHashMap<>();
-
-//        MqttStatusReceiver statusTopic = new MqttStatusReceiver(
-//                brokerURL,
-//                "speed-app-client-stat",
-//                "+/status",
-//                queueDeviceStatusTopicData);
-
-//        DeviceStatusUpdater deviceStatusUpdater = new DeviceStatusUpdater(
-//                queueDeviceStatusTopicData,
-//                devicesStatus,
-//                queueDeviceStatusUpdate,
-//                deviceDataCache
-//        );
-
-//        Thread statusUpdateThread = new Thread(deviceStatusUpdater);
-
-//        statusUpdateThread.start();
 
         MqttDetectionReceiver detectionTopic1 = new MqttDetectionReceiver(
                 brokerURL,
@@ -63,11 +46,9 @@ public class Main {
 
         Thread t_detection1 = new Thread(detectionTopic1);
         Thread t_detection2 = new Thread(detectionTopic2);
-//        Thread t_status = new Thread(statusTopic);
 
         t_detection1.start();
         t_detection2.start();
-//        t_status.start();
 
         boolean speedCalcRunning = false;
 
@@ -83,6 +64,10 @@ public class Main {
         Thread vehicleSpeedThread = new Thread(vehicleSpeedCalculator);
         vehicleSpeedThread.start();
 
+        DataServer dataServer = new DataServer(dataServerStartRow);
+        Thread dataServerThread = new Thread(dataServer);
+        dataServerThread.start();
+
         while(true) {
             speedCalcRunning = true;
 
@@ -90,10 +75,6 @@ public class Main {
                 while(queueDeviceSpeed.peek() != null){
                     System.out.println("Speed: " + queueDeviceSpeed.poll() + " m/s");
                 }
-//                System.out.println("Devices:");
-//                deviceDataCache.forEach((k, v) -> {
-//                    System.out.println(" - " + k + ": " + v);
-//                });
                 Thread.sleep(5000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
--- src/main/java/hr/fer/dippro/server/DataServer.java
+++ src/main/java/hr/fer/dippro/server/DataServer.java
@@ -1,13 +1,50 @@
 package hr.fer.dippro.server;
 
+import io.javalin.Javalin;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
 public class DataServer implements Runnable {
+    private final int startRow;
+    private int currentRow;
 
-    public DataServer(int startRow, int reading_period) {
-
+    public DataServer(int startRow) {
+        this.startRow = startRow;
+        this.currentRow = startRow;
     }
 
     @Override
     public void run() {
+        Javalin app = Javalin.create().start(8080);
 
+        // A dynamic path parameter
+        app.get("/sensor-data", ctx -> {
+            System.out.println("GET /sensor-data {resp_line: " + currentRow + "}");
+            ctx.result(getNewLine());
+        });
+    }
+
+    private String getNewLine() throws IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("csv/vel.csv");
+
+        String processedContent = "";
+
+        assert inputStream != null;
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+
+            processedContent = reader.lines()
+                    .skip(currentRow)
+                    .limit(1)
+                    .collect(Collectors.joining(""));
+
+            currentRow++;
+        }
+
+        return processedContent;
     }
 }
